25422f19c69a3aa6e78a2accf8142b512e237b04
ranzhouli <874177815@qq.com>
2017-12-25 23:41:51 +0800

    format

--- game1/ball.js
+++ game1/ball.js
@@ -1,36 +1,36 @@
-        //球
-        var Ball = function () {
-            var image = imageFromPath("ball.png");
+//球
+var Ball = function () {
+    var image = imageFromPath("ball.png");
 
-            var o = {
-                image: image,
-                x: 100,
-                y: 200,
-                speedX: 5,
-                speedY: 5,
-                fired: false
-            };
+    var o = {
+        image: image,
+        x: 100,
+        y: 200,
+        speedX: 5,
+        speedY: 5,
+        fired: false
+    };
 
-            o.fire = function () {
-                o.fired = true;
+    o.fire = function () {
+        o.fired = true;
+    }
+
+    o.move = function () {
+        if (o.fired) {
+            if (o.x < 0 || o.x > 400) {
+                o.speedX *= -1;
             }
-
-            o.move = function () {
-                if (o.fired) {
-                    if (o.x < 0 || o.x > 400) {
-                        o.speedX *= -1;
-                    }
-                    if (o.y < 0 || o.y > 300) {
-                        o.speedY *= -1;
-                    }
-
-                    //move
-                    o.x += o.speedX;
-                    o.y += o.speedY;
-                }
-            }
-            o.rebound = function () {
+            if (o.y < 0 || o.y > 300) {
                 o.speedY *= -1;
             }
-            return o;
-        };
+
+            //move
+            o.x += o.speedX;
+            o.y += o.speedY;
+        }
+    }
+    o.rebound = function () {
+        o.speedY *= -1;
+    }
+    return o;
+};
--- game1/block.js
+++ game1/block.js
@@ -1,23 +1,23 @@
-        //砖块
-        var Block = function () {
-            var image = imageFromPath("block.png");
+//砖块
+var Block = function () {
+    var image = imageFromPath("block.png");
 
-            var o = {
-                image: image,
-                x: 100,
-                y: 100,
-                w: 50,
-                h: 20,
-                alive: true,
-            };
+    var o = {
+        image: image,
+        x: 100,
+        y: 100,
+        w: 50,
+        h: 20,
+        alive: true,
+    };
 
-            o.kill = function () {
-                o.alive = false;
-            };
+    o.kill = function () {
+        o.alive = false;
+    };
 
-            o.collide = function (ball) {
-                return o.alive && (rectIntersects(o, ball) || rectIntersects(ball, o));
-            };
+    o.collide = function (ball) {
+        return o.alive && (rectIntersects(o, ball) || rectIntersects(ball, o));
+    };
 
-            return o;
-        };
+    return o;
+};
