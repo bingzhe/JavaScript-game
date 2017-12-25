633d04be79f1a709d1a726d7d0fe8975b72bde18
ranzhouli <874177815@qq.com>
2017-12-25 23:40:22 +0800

    拆分

---
+++ game1/block.js
@@ -0,0 +1,23 @@
+        //砖块
+        var Block = function () {
+            var image = imageFromPath("block.png");
+
+            var o = {
+                image: image,
+                x: 100,
+                y: 100,
+                w: 50,
+                h: 20,
+                alive: true,
+            };
+
+            o.kill = function () {
+                o.alive = false;
+            };
+
+            o.collide = function (ball) {
+                return o.alive && (rectIntersects(o, ball) || rectIntersects(ball, o));
+            };
+
+            return o;
+        };
---
+++ game1/guagame.js
@@ -0,0 +1,48 @@
+var GuaGame = function () {
+    var g = {
+        actions: {},
+        keydowns: {},
+    };
+
+    var canvas = document.querySelector("#id-canvas");
+    var context = canvas.getContext("2d");
+    g.canvas = canvas;
+    g.context = context;
+
+    //draw
+    g.drawImage = function (guaImage) {
+        g.context.drawImage(guaImage.image, guaImage.x, guaImage.y);
+    };
+    //events
+    window.addEventListener("keydown", function (event) {
+        g.keydowns[event.key] = true;
+    });
+    window.addEventListener("keyup", function (event) {
+        g.keydowns[event.key] = false;
+    });
+
+    g.registerAction = function (key, cb) {
+        g.actions[key] = cb;
+    };
+
+    //timer
+    setInterval(function () {
+        //events
+        var actions = Object.keys(g.actions);
+        for (var i = 0; i < actions.length; i++) {
+            var key = actions[i];
+            if (g.keydowns[key]) {
+                //如果按键被按下，调用注册的action
+                g.actions[key]();
+            }
+        }
+        //update
+        g.update();
+        //clear
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        //draw
+        g.draw();
+    }, 1000 / 60);
+
+    return g;
+};
