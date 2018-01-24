d2fe2de294521d87cbea570bf8d01c9f8d3214ac
ranzhouli <874177815@qq.com>
2018-01-24 23:55:05 +0800

    加载资源

--- game1/guagame.js
+++ game1/guagame.js
@@ -1,74 +1,84 @@
-var GuaGame = function (fps) {
-    var g = {
-        actions: {},
-        keydowns: {},
-    };
+var GuaGame = function(fps, images) {
+  //images 是 array,里面是图片的名字
+  var g = {
+    actions: {},
+    keydowns: {}
+  };
 
-    var canvas = document.querySelector("#id-canvas");
-    var context = canvas.getContext("2d");
-    g.canvas = canvas;
-    g.context = context;
+  var canvas = document.querySelector("#id-canvas");
+  var context = canvas.getContext("2d");
+  g.canvas = canvas;
+  g.context = context;
 
-    //draw
-    g.drawImage = function (guaImage) {
-        g.context.drawImage(guaImage.image, guaImage.x, guaImage.y);
-    };
+  //draw
+  g.drawImage = function(guaImage) {
+    g.context.drawImage(guaImage.image, guaImage.x, guaImage.y);
+  };
+  //events
+  window.addEventListener("keydown", function(event) {
+    g.keydowns[event.key] = true;
+  });
+  window.addEventListener("keyup", function(event) {
+    g.keydowns[event.key] = false;
+  });
+
+  g.registerAction = function(key, cb) {
+    g.actions[key] = cb;
+  };
+
+  window.fps = 30;
+
+  var runloop = function() {
     //events
-    window.addEventListener("keydown", function (event) {
-        g.keydowns[event.key] = true;
-    });
-    window.addEventListener("keyup", function (event) {
-        g.keydowns[event.key] = false;
-    });
-
-    g.registerAction = function (key, cb) {
-        g.actions[key] = cb;
-    };
-
-    window.fps = 30;
-    
-    var runloop = function () {
-        //events
-        var actions = Object.keys(g.actions);
-        for (var i = 0; i < actions.length; i++) {
-            var key = actions[i];
-            if (g.keydowns[key]) {
-                //如果按键被按下，调用注册的action
-                g.actions[key]();
-            }
-        }
-        //update
-        g.update && g.update();
-        //clear
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        //draw
-        g.draw && g.draw();
-        //next run loop
-        setTimeout(function () {
-            runloop();
-        }, 1000 / window.fps)
+    var actions = Object.keys(g.actions);
+    for (var i = 0; i < actions.length; i++) {
+      var key = actions[i];
+      if (g.keydowns[key]) {
+        //如果按键被按下，调用注册的action
+        g.actions[key]();
+      }
     }
+    //update
+    g.update && g.update();
+    //clear
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    //draw
+    g.draw && g.draw();
+    //next run loop
+    setTimeout(function() {
+      runloop();
+    }, 1000 / window.fps);
+  };
 
-    runloop();
+  for (var i = 0; i < images.length; i++) {
+    var path = images[i];
+    var img = new Images();
+    img.src = path;
+    img.onload = function() {
+        //所有图片都载入成功之后，调用run
+    };
+  }
 
-    // //timer
-    // setInterval(function () {
-    //     //events
-    //     var actions = Object.keys(g.actions);
-    //     for (var i = 0; i < actions.length; i++) {
-    //         var key = actions[i];
-    //         if (g.keydowns[key]) {
-    //             //如果按键被按下，调用注册的action
-    //             g.actions[key]();
-    //         }
-    //     }
-    //     //update
-    //     g.update && g.update();
-    //     //clear
-    //     context.clearRect(0, 0, canvas.width, canvas.height);
-    //     //draw
-    //     g.draw && g.draw();
-    // }, 1000 / fps);
+  runloop();
 
-    return g;
+  // //timer
+  // setInterval(function () {
+  //     //events
+  //     var actions = Object.keys(g.actions);
+  //     for (var i = 0; i < actions.length; i++) {
+  //         var key = actions[i];
+  //         if (g.keydowns[key]) {
+  //             //如果按键被按下，调用注册的action
+  //             g.actions[key]();
+  //         }
+  //     }
+  //     //update
+  //     g.update && g.update();
+  //     //clear
+  //     context.clearRect(0, 0, canvas.width, canvas.height);
+  //     //draw
+  //     g.draw && g.draw();
+  // }, 1000 / fps);
+
+  return g;
 };
